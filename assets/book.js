62ed244ff0af4a1993ec13672a5a61f22a9cb3e8
Andrew MacLachlan <51865303+andrewmaclachlan@users.noreply.github.com>
2020-05-12 12:36:28 +0100

    css fixes

--- assets/book.js
+++ assets/book.js
@@ -6,19 +6,3 @@ $(document).ready(function() {
     $(this).addClass("hasAnchor").prepend('<a href="' + anchor + '" class="anchor"></a>');
   });
 });
-$(document).ready(function() {
-
-  // Copy Button
-  $chunks = $('pre.sourceCode > code.sourceCode');
-  $chunks.each(function(i, val) {
-    $(this).prepend("<button class=\"button copy\"><i class=\"fa fa-copy fa-2x\"></i></button>").click(function() {
-      var $temp = $("<textarea>");
-      $("body").append($temp);
-      var content = $(this).clone().children("button").remove().end().text();
-      $temp.val(content).select();
-      document.execCommand("copy");
-      $temp.remove();
-    });
-  });
-  
-});
